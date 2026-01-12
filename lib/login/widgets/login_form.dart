60c94c4143d3d10d8baf19e2f4cc9030960e5573
ChandanaThoti <chandana.thoti@everest.engineering>
2026-01-12 13:46:34 +0530

    Add login form with fields and button

---
+++ lib/login/widgets/login_form.dart
@@ -0,0 +1,35 @@
+import 'package:everest_engineering/login/widgets/login_fields.dart';
+import 'package:flutter/material.dart';
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Padding(
+        padding: EdgeInsets.only(top: 50, left: 20, right: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.end,
+          spacing: 25,
+          children: [
+            LoginFields(),
+            ElevatedButton(
+              onPressed: () => {},
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all<Color>(
+                  const Color.fromARGB(255, 33, 32, 32),
+                ),
+                foregroundColor: WidgetStateProperty.all<Color>(
+                  const Color.fromARGB(255, 245, 243, 243),
+                ),
+              ),
+              child: Text("Login"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
