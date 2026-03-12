429109c56b5be86705700633885d338966c71a33
ChandanaThoti <chandana.thoti@everest.engineering>
2026-03-12 09:53:55 +0530

    Add stateful widget in login form

--- lib/widgets/login_form.dart
+++ lib/widgets/login_form.dart
@@ -1,8 +1,53 @@
-import 'package:everest_engineering/widgets/login_fields.dart';
 import 'package:flutter/material.dart';
 
-class LoginForm extends StatelessWidget {
-  const LoginForm({super.key});
+class LoginState extends StatefulWidget {
+  const LoginState({super.key});
+  @override
+  State<LoginState> createState() => _LoginForm();
+}
+
+class _LoginForm extends State<LoginState> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  void _login() {
+    String username = _usernameController.text;
+    String password = _passwordController.text;
+
+    if (username != '' && password != '') {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text("Confirm to Login"),
+          content: const Text("Login Successful"),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text("okay"),
+            ),
+          ],
+        ),
+      );
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          content: const Text("Please fill all the fields"),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text("okay"),
+            ),
+          ],
+        ),
+      );
+    }
+    _usernameController.clear();
+    _passwordController.clear();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,25 +59,24 @@ class LoginForm extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.end,
           spacing: 25,
           children: [
-            LoginFields(),
+            TextFormField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                hintText: "Chandana",
+                labelText: "User Name",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            TextFormField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                hintText: "password@123",
+                labelText: "Password",
+                border: OutlineInputBorder(),
+              ),
+            ),
             ElevatedButton(
-              onPressed: () => {
-                showDialog(
-                  context: context,
-                  builder: (context) => AlertDialog(
-                    title: const Text("Confirm to Login"),
-                    content: const Text("Login Successful"),
-                    actions: <Widget>[
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text("okay"),
-                      ),
-                    ],
-                  ),
-                ),
-              },
+              onPressed: () => _login(),
               style: ButtonStyle(
                 backgroundColor: WidgetStateProperty.all<Color>(
                   const Color.fromARGB(255, 33, 32, 32),
